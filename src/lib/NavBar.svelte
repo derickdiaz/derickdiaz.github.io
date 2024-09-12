e0d98a49f5925cfd7f30479cb3f6f968d999bf95
Derick Diaz <derickdiaz123@outlook.com>
2024-09-12 12:09:17 -0500

    Added CTA that the site is under construction

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,50 @@
+<nav>
+    <div class="left-nav centered">
+        <h1>Derick Diaz</h1>
+    </div>
+    <div class="right-nav">
+        <ul>
+            <li class="centered active">Home</li>
+            <li class="centered">Contact Me</li>
+        </ul>
+    </div>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 20px;
+        background-color: white;
+        filter: drop-shadow(0px 5px 10px lightgray)
+    }
+
+    .right-nav > ul {
+        display: flex;
+        justify-content: space-between;
+        gap: 20px;
+    }
+
+    .active {
+        background-color: #2DA5EB;
+        color: white;
+    }
+
+    .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .right-nav > ul > li {
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .right-nav > ul > li:hover {
+        background-color: #2DA5EB;
+        color: white;
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,26 @@
+<script>
+    import NavBar from "../lib/NavBar.svelte";
+
+</script>
 <div class="content">
-    <h1>This site is under construction...</h1>
+    <NavBar />
+    <div class="cta">
+        <h1>
+            Won't Lie...<br>
+            This site is under construction...<br>
+            I am commiting to one github commit a day to get it where it needs to be.<br>
+            So check in every once in a while to see something different.
+        </h1>
+    </div>
 </div>
+
+<style>
+    .cta {
+        padding-top: 30px;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 2em;
+    }
+</style>
