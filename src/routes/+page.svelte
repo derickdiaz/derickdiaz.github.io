f137bad6314b028a517956812ec8b7cc487f7303
Derick Diaz <derickdiaz123@outlook.com>
2025-06-02 22:56:54 -0500

    Added squares

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,64 @@
     import NavBar from "../lib/NavBar.svelte";
 
 </script>
-<div class="content">
-    <NavBar />
-    <div class="cta">
-        <h1>
-            Won't Lie...<br>
-            This site is under construction...<br>
-            I am commiting to one github commit a day to get it where it needs to be.<br>
-            So check in every once in a while to see something different.
-        </h1>
+<main>
+    <h1>Derick Diaz</h1>
+    <hr/>
+    <div class="content-grid">
+        <div class="content-item profile pad-10">
+            <div class="gray">
+                
+            </div>
+        </div>
+        <div class="content-item info-grid">
+            <div class="content-item pad-10">
+                <div class="gray" />
+            </div>
+            <div class="content-item pad-10">
+                <div class="gray" />
+            </div>
+        </div>
     </div>
-</div>
+</main>
 
 <style>
-    .cta {
-        padding-top: 30px;
-        text-align: center;
+    h1 {
+        color: white;
+        font-size: 3rem;
     }
 
-    h1 {
-        font-size: 2em;
+    hr {
+        margin-bottom: 30px;
+        position: relative;
+        /* transform: translateY(-10px); */
+    }
+
+    .content-grid {
+        /* border: 1px solid white; */
+        height: 500px;
+        display: grid;
+        grid-template-columns: 40% auto;
+        gap:10px;
+    }
+
+    .content-item {
+        /* border: 1px solid white; */
+        gap: 10px;
+    }
+
+    .info-grid {
+        display: grid;
+        grid-template-rows: auto auto;
+    }
+
+    .gray {
+        border: 1px solid gray;
+        height: 100%;
+        border-radius: 20px;
+        transition: all 500ms;
+    }
+
+    .gray:hover {
+        transform: scale(1.02);
     }
 </style>
